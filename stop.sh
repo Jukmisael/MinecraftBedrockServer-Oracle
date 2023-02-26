d59506b52c73d05996966b6070ba30e845e0f9fa
Misael Filho <jukmisael@gmail.com>
2023-02-26 20:19:41 -0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -18,7 +18,7 @@ if [[ $(id -u) = 0 ]]; then
 fi
 
 # Check if server is running
-if ! screen -list | grep -q '\.servername\s'; then
+if ! tmux list-sessions | grep -q '\.servername\s'; then
   echo "Server is not currently running!"
   exit 1
 fi
@@ -72,7 +72,7 @@ screen -Rd servername -X stuff "stop$(printf '\r')"
 # Wait up to 20 seconds for server to close
 StopChecks=0
 while [[ $StopChecks -lt 20 ]]; do
-  if ! screen -list | grep -q '\.servername\s'; then
+  if ! tmux list-sessions | grep -q '\.servername\s'; then
     break
   fi
   sleep 1
@@ -80,7 +80,7 @@ while [[ $StopChecks -lt 20 ]]; do
 done
 
 # Force quit if server is still open
-if screen -list | grep -q '\.servername\s'; then
+if tmux list-sessions | grep -q '\.servername\s'; then
   echo "Minecraft server still hasn't stopped after 20 seconds, closing screen manually"
   screen -S servername -X quit
 fi
